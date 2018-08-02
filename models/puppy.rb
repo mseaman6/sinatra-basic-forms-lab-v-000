b0b5e2fdeb93381e6dbe95f56632c027eae2623b
Monica Seaman <monica.l.seaman@gmail.com>
2018-08-02 15:55:09 +0000

    Done.

---
+++ models/puppy.rb
@@ -0,0 +1,16 @@
+class Puppy
+  attr_accessor :name, :breed, :age
+
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+  end
+
+  #def initialize(params)
+  #  @name = params[:name]
+  #  @breed = params[:breed]
+  #  @age = params[:age]
+  #end
+
+end
